7f050c970f9034937173a767db4371d906cb5e3d
Marina Torres <marilimatorres26@gmail.com>
2024-08-27 21:02:52 -0300

    nova corrente

--- scripts/player2.gd
+++ scripts/player2.gd
@@ -4,19 +4,18 @@ const WALK_SPEED = 100.0
 const RUN_SPEED = 200.0
 const JUMP_FORCE = -500.0
 
-const SCREEN_HEIGHT = 1152  # Ajuste conforme necessário para corresponder à altura da tela
+const SCREEN_HEIGHT = 600  # Ajuste conforme necessário para corresponder à altura da tela
 
 # Obtém a gravidade das configurações do projeto.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var is_jumping := false
 @onready var animation := $animCapivara as AnimatedSprite2D  # Ajuste se necessário para o seu nó de animação
 @onready var chain := $"../Chain" as Node2D  # Referência para a corrente
-@onready var jump_sound = $jump
 
 func _process(delta):
 	# Verifica se o personagem caiu fora da tela
 	if position.y > SCREEN_HEIGHT:
-		die()  # Substitua por sua lógica de game over
+		get_tree().change_scene("res://scenes/main.tscn") 
 
 func _physics_process(delta):
 	# Adiciona a gravidade
@@ -25,7 +24,6 @@ func _physics_process(delta):
 
 	# Manipula o pulo
 	if Input.is_action_just_pressed("w") and is_on_floor() and not is_chain_stretched():
-		jump_sound.play()
 		velocity.y = JUMP_FORCE
 		is_jumping = true
 	elif is_on_floor():
@@ -65,5 +63,7 @@ func _physics_process(delta):
 func is_chain_stretched() -> bool:
 	return chain and position.distance_to(chain.player1.position) > (chain.chain_length * 20)
 
-func die():
-	queue_free()  # Remove o personagem da cena, substitua conforme a lógica de game over
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("players"):
+		get_tree().change_scene("res://scenes/main.tscn")
